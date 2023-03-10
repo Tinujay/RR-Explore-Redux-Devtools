c507048553aad159531b17cc0ef8c435f0e112d5
run <tinuvazhayil@gmail.com>
2023-03-09 21:33:07 -0500

    completed

--- src/Todo.js
+++ src/Todo.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { addTodo, removeOne, clearTodo } from './features/todoSlice'
+//
 
 function Todo() {
     const items = useSelector((state) => state.todos.items)
